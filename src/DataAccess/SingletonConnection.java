e54b567ec6c01d01361c80977130ab4a16ec0bd4
simon <etu55663@henallux.be>
2025-05-25 22:04:04 +0200

    j'ai du réinstaller tt mysql, nv mdp et nv nom de bd -> "bd_projet"

--- src/DataAccess/SingletonConnection.java
+++ src/DataAccess/SingletonConnection.java
@@ -7,7 +7,7 @@ public class SingletonConnection {
     private SingletonConnection(){}
     public static Connection getInstance() throws SQLException {
         if (connectionUnique == null || connectionUnique.isClosed()) {
-            connectionUnique = DriverManager.getConnection("jdbc:mysql://localhost:3306/video_game_store_db", "root", "mdp");
+            connectionUnique = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_projet", "root", "mdp1");
         }
         return connectionUnique;
     }
