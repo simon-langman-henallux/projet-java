a5a7da9faae27939180558979ac1c83ce9c97d8b
simon <etu55663@henallux.be>
2025-08-02 22:27:40 +0200

    ajout de int streetNumber aux attributs de Person et à ses constructeurs car oubli....

--- src/dataAccess/PersonDAO.java
+++ src/dataAccess/PersonDAO.java
@@ -3,10 +3,7 @@ package dataAccess;
 import exception.DataAccessException;
 import model.Person;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 public class PersonDAO implements IPersonDAO {
@@ -21,23 +18,43 @@ public class PersonDAO implements IPersonDAO {
         }
     }
 
-    public void insert(Person person) throws SQLException {
-        String sql = "insert into person (name, firstName, phoneNumber, birthDate, boxNumber, accountNumber, streetName, streetNumber, isClient, isSupplier, zipCodeCity, nameCity, country) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        try (PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setString(1, game.getTitle());
-            ps.setDouble(2, game.getPrice());
-            ps.setDate(3, (Date) game.getReleaseDate());
-            ps.setString(4, game.getDescription());
-            ps.setInt(5, game.getAgeRestriction());
-            ps.setBoolean(6, game.isMultiplayer());
-            ps.setDouble(7, game.getDuration());
-            ps.setString(8, game.getPublisher().toString());
-            ps.setString(9, game.getGenre().toString());
-            ps.executeUpdate();
+    public void insert(Person person) {
+        String sql = """
+        INSERT INTO person
+          (name, firstName, phoneNumber, birthDate, boxNumber,
+           accountNumber, streetName, streetNumber,
+           isClient, isSupplier, zipCodeCity, nameCity, country)
+        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)
+        """;
+        try (PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, person.getName());
+            ps.setString(2, person.getFirstName());
+            ps.setString(3, person.getPhoneNumber());
+            ps.setDate(4, new java.sql.Date(person.getBirthDate().getTime()));
+            ps.setObject(5, person.getBoxNumber(), Types.INTEGER);
+            ps.setString(6, person.getAccountNumber());
+            ps.setString(7, person.getStreetName());
+            ps.setInt(8, person.getStreetNumber());
+            ps.setBoolean(9, person.isClient());
+            ps.setObject(10, person.isSupplier(), Types.BOOLEAN);
+            ps.setInt(11, person.getZipCodeCity());
+            ps.setString(12, person.getNameCity());
+            ps.setString(13, person.getCountry());
+
+            int affected = ps.executeUpdate();
+            if (affected == 0) {
+                throw new DataAccessException("Échec de l’insertion de Person");
+            }
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    person.setId(rs.getInt(1));
+                }
+            }
         } catch (SQLException e) {
-            throw new DataAccessException("Insert Person Error");
+            throw new DataAccessException("Insert Person Error", e);
         }
     }
+
     public Person find(int id) throws SQLException { return null; }
     public List<Person> findAll() throws SQLException { return null; }
     public void update(Person person) throws SQLException {}
--- src/model/Person.java
+++ src/model/Person.java
@@ -4,12 +4,12 @@ import java.util.Date;
 
 public class Person {
 
-    private int id, boxNumber, zipCodeCity;
+    private int id, boxNumber, streetNumber, zipCodeCity;
     private String name, firstName, phoneNumber, accountNumber, streetName, nameCity, country;
     private Date birthDate;
     private boolean isClient, isSupplier;
     // constructeur utile pour les delete, update etc...
-    public Person(int id, String name, String firstName, String phoneNumber, Date birthDate, int boxNumber, String accountNumber, String streetName, boolean isClient, boolean isSupplier, int zipCodeCity, String nameCity, String country) {
+    public Person(int id, String name, String firstName, String phoneNumber, Date birthDate, int boxNumber, String accountNumber, String streetName, int streetNumber, boolean isClient, boolean isSupplier, int zipCodeCity, String nameCity, String country) {
         this.id = id;
         this.name = name;
         this.firstName = firstName;
@@ -18,6 +18,7 @@ public class Person {
         this.boxNumber = boxNumber;
         this.accountNumber = accountNumber;
         this.streetName = streetName;
+        this.streetNumber = streetNumber;
         this.isClient = isClient;
         this.isSupplier = isSupplier;
         this.zipCodeCity = zipCodeCity;
@@ -25,7 +26,7 @@ public class Person {
         this.country = country;
     }
     // constructeur pour les insert car id auto-incrémenter
-    public Person(String name, String firstName, String phoneNumber, Date birthDate, int boxNumber, String accountNumber, String streetName, boolean isClient, boolean isSupplier, int zipCodeCity, String nameCity, String country) {
+    public Person(String name, String firstName, String phoneNumber, Date birthDate, int boxNumber, String accountNumber, String streetName, int streetNumber, boolean isClient, boolean isSupplier, int zipCodeCity, String nameCity, String country) {
         this.name = name;
         this.firstName = firstName;
         this.phoneNumber = phoneNumber;
@@ -33,6 +34,7 @@ public class Person {
         this.boxNumber = boxNumber;
         this.accountNumber = accountNumber;
         this.streetName = streetName;
+        this.streetNumber = streetNumber;
         this.isClient = isClient;
         this.isSupplier = isSupplier;
         this.zipCodeCity = zipCodeCity;
@@ -41,103 +43,107 @@ public class Person {
     }
 
     public Person() {
-        this(0, null, null, null, null, 0, null, null, false, false, 0, null, null);
+        this(null, null, null, null, 0, null, null, 0, false, false, 0, null, null);
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public int getId() {
+        return id;
     }
 
     public int getBoxNumber() {
         return boxNumber;
     }
 
-    public void setBoxNumber(int boxNumber) {
-        boxNumber = boxNumber;
-    }
-
     public int getZipCodeCity() {
         return zipCodeCity;
-    }
-
-    public void setZipCodeCity(int zipCodeCity) {
-        zipCodeCity = zipCodeCity;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        name = name;
-    }
-
     public String getFirstName() {
         return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        firstName = firstName;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        phoneNumber = phoneNumber;
-    }
-
     public String getAccountNumber() {
         return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        accountNumber = accountNumber;
     }
 
     public String getStreetName() {
         return streetName;
     }
 
-    public void setStreetName(String streetName) {
-        streetName = streetName;
-    }
-
     public String getNameCity() {
         return nameCity;
-    }
-
-    public void setNameCity(String nameCity) {
-        nameCity = nameCity;
     }
 
     public String getCountry() {
         return country;
     }
 
-    public void setCountry(String country) {
-        country = country;
-    }
-
     public Date getBirthDate() {
         return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        birthDate = birthDate;
     }
 
     public boolean isClient() {
         return isClient;
     }
 
-    public void setClient(boolean client) {
-        isClient = client;
-    }
-
     public boolean isSupplier() {
         return isSupplier;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBoxNumber(int boxNumber) {
+        this.boxNumber = boxNumber;
+    }
+
+    public void setZipCodeCity(int zipCodeCity) {
+        this.zipCodeCity = zipCodeCity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public void setNameCity(String nameCity) {
+        this.nameCity = nameCity;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setClient(boolean client) {
+        isClient = client;
     }
 
     public void setSupplier(boolean supplier) {
